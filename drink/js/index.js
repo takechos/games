0423b71693be40878d4d98b51aad316d4305cc22
TakeruHaruki <85802216+takechos@users.noreply.github.com>
2023-03-17 12:39:24 +0900

    modify choices

--- drink/js/index.js
+++ drink/js/index.js
@@ -9,8 +9,9 @@ var spotList = [];
 // 行きたい場所を配列にぶち込む
 spotList = [
     '<span>たける</span><br>飲んでなくない？',
-    '<span>かすみ</span><br>飲んでなくない？',
+    '<span>れん</span><br>飲んでなくない？',
     '<span>しゅーま</span><br>飲んでなくない？',
+    '<span>なおちゃん</span><br>飲んでなくない？',
     '<span>ゆうな</span><br>飲んでなくない？',
     '<span>全員</span><br>飲んでなくない？'
                 ];
